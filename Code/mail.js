7976c3db82603f153df10b2ee239a7af3848471f
Ludvig Hammarstedt <ludvig.hammarstedt@skola.taby.se>
2022-05-20 13:41:44 +0200

    done

---
+++ Code/mail.js
@@ -0,0 +1,91 @@
+
+
+function validateName() {
+
+    var name = document.getElementById('contact-name').value;
+  
+    if(name.length == 0) {
+  
+      producePrompt('Name is required', 'name-error' , 'maroon')
+      return false;
+  
+  }
+  
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+  
+      producePrompt('First and last name, please.','name-error', 'maroon');
+      return false;
+  
+  }
+  
+  producePrompt('Valid', 'name-error', 'green');
+  return true;
+  
+  }
+  
+  function validateEmail () {
+  
+  var email = document.getElementById('contact-email').value;
+  
+  if(email.length == 0) {
+  
+  producePrompt('Email Invalid','email-error', 'maroon');
+  return false;
+  
+  }
+  
+  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+  
+  producePrompt('Email Invalid', 'email-error', 'maroon');
+  return false;
+  
+  }
+  
+  producePrompt('Valid', 'email-error', 'green');
+  return true;
+  
+  }
+  
+  function validateMessage() {
+  var message = document.getElementById('contact-message').value;
+  var required = 30;
+  var left = required - message.length;
+  
+  if (left > 0) {
+  producePrompt(left + ' more characters required','message-error','maroon');
+  return false;
+  }
+  
+  producePrompt('Valid', 'message-error', 'green');
+  return true;
+  
+  }
+  
+  function validateForm() {
+  if (!validateName() || !validateEmail() || !validateMessage()) {
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'maroon');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+  return false;
+  }
+  else {
+  
+  }
+  }
+  
+  function jsShow(id) {
+  document.getElementById(id).style.display = 'block';
+  }
+  
+  function jsHide(id) {
+  document.getElementById(id).style.display = 'none';
+  }
+  
+  
+  function producePrompt(message, promptLocation, color) {
+  
+  document.getElementById(promptLocation).innerHTML = message;
+  document.getElementById(promptLocation).style.color = color;
+  
+  }
+  
